a7913c9410d3df4d1ba3975cf33c1fcfbd6abefb
xiaoxie <2215775451@qq.com>
2023-04-17 09:56:20 +0800

    搭了个架子

---
+++ config/default.js
@@ -0,0 +1,5 @@
+const config={
+    //启动的端口号
+    port:3000
+}
+module.exports = config
--- index.js
+++ index.js
@@ -13,15 +13,34 @@ const cors = require('cors');
   app.use(express.static(__dirname+'/dist'));
   //访问存储图片的文件夹
   app.use(express.static(__dirname+'/data'));
+  app.use(express.static(__dirname+'/views'));
 
 
 
   //设置接受所有请求
   app.use(cors());
 
-  //只接受我的请求
+  //只接受我网站的请求
 //   const corsOptions = {
 //     origin: 'http://ccmwn.cn'
 //   };
-
 //   app.use(cors(corsOptions))
+
+//修改模板文件后缀名为html
+app.engine('html',ejs.__express);
+app.set('view engine','html');
+
+//自动解析前端数据
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+ 
+//引入路由
+
+//require('./routes/index')(app);
+// require('./routes/files')(app);
+
+//监听启动服务器
+app.listen(config.port,()=>{
+  console.log(`${config.port}端口被启动了,http://127.0.0.1:3000`);
+});
+
